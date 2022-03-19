d7c9f6c9b177af8ea5fc17f039c3e4fc4bf2af88
hosseinvejdani <vejdani.shojae@gmail.com>
2022-03-19 09:13:30 +0330

    add svg to png converter

--- mergesvg.py
+++ mergesvg.py
@@ -1,4 +1,5 @@
 import svgutils.transform as st
+from cairosvg import svg2png
 
 background = st.fromfile('files/test-1.svg')
 top1 = st.fromfile('files/test-2.svg')
@@ -6,3 +7,7 @@ top2 = st.fromfile('files/test-3.svg')
 background.append(top1)
 background.append(top2)
 background.save('result.svg')
+
+# convert svg to png file
+svg2png(url='result.svg', write_to='result.png', dpi=72,
+        output_height=600, output_width=600)
